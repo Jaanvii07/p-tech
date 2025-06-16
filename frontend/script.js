ded93e580bffe38fd0b3e716735532d6134afe15
janvi <janvilakhanpatidar@gmail.com>
2025-06-16 22:53:45 +0530

    guide

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,5 @@
-// JavaScript for Sidebar Toggle
+
 document.getElementById("toggleMenu").addEventListener("click", function () {
     document.getElementById("sideNav").classList.toggle("open");
 });
+
