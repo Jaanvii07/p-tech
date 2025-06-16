ab4811d81e4c5d2d52bb191f1a90c386494781c3
Jaysolanki7 <js9302244036@gmail.com>
2025-06-16 10:56:27 +0530

    .

---
+++ frontend/script.js
@@ -0,0 +1,4 @@
+// JavaScript for Sidebar Toggle
+document.getElementById("toggleMenu").addEventListener("click", function () {
+    document.getElementById("sideNav").classList.toggle("open");
+});
