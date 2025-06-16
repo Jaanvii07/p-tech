40e162bfc15cc387203227bddee601317fe1df5a
Jaysolanki7 <js9302244036@gmail.com>
2025-06-16 10:58:17 +0530

    update

--- frontend/script.js
+++
@@ -1,4 +0,0 @@
-// JavaScript for Sidebar Toggle
-document.getElementById("toggleMenu").addEventListener("click", function () {
-    document.getElementById("sideNav").classList.toggle("open");
-});
